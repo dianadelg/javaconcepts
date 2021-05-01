f4bdd47d88d5f22d9ce83f91e643dfbe43a06053
Diana Del Gaudio <ddelgaudio347@gmail.com>
2021-05-01 18:46:26 -0400

    leeeeeetcode

---
+++ data_structures/src/leetcode/ReverseInteger.java
@@ -0,0 +1,53 @@
+package leetcode;
+/*
+ * Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+ Testcases:
+ 123
+-123
+120
+0
+
+Your runtime --> iterating through loop that takes O(log n) time because there are roughly log_10(n) digits in n
+and all of the checks we do are constant and not contingent on input size
+Your space --> O(1), not contingent on input size, will take the same time regardless of digits
+ 
+ */
+public class ReverseInteger {
+	public static void main (String[]args) {
+		int num = -2147483412;
+		System.out.println(Reverse(num));
+	}
+	public static int Reverse(int x) {
+	        long revNum = 0;
+	        boolean isNegative=false;
+	        if(x<0){
+	            isNegative=true;
+	            x=x*(-1);
+	        }
+	        if(x==0){
+	            return 0;
+	        }
+	        int length = (int) (Math.log10(x) + 1);
+	        for(int i = length-1; i>=0;i--){
+	            //124 length = 3
+	        	
+	            revNum += ((Math.pow(10,i))*(x%(10)));
+	            x=x/10;           
+	        }
+	        
+	        if (revNum < Integer.MIN_VALUE || revNum > Integer.MAX_VALUE) {
+	            return 0;
+	        } else {
+	            if(isNegative==true){
+	                return (int)revNum*(-1);
+	            }else{
+	                return (int)revNum;
+	            }
+	        }
+	    }
+	
+}
---
+++ data_structures/src/leetcode/TwoSum.java
@@ -0,0 +1,57 @@
+package leetcode;
+/*
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+ */
+
+/*
+ * Test Cases:
+ * [2,7,11,15]
+	9
+	[3,2,4]
+	6
+	[3,3]
+	6
+	
+	Your own cases:
+	//[1,2] target 3
+    //[1,1,2] target 3
+    //[1,0,5,2] target 3
+    //[0,1,5,2,4]
+    //[0,8,2,1]
+     * 
+     * Your runtime = O(n^2) ehhh --> I think there was a way to do it with hashmaps but im tired
+     * Your space = O(n) for new array --> optimize by just returning return new int [] = {i,y}; then it is O(1)
+ */
+public class TwoSum {
+	public static void main (String[]args) {
+		int[] arr = {1,2,3};
+		int[] solution = twoSum(arr,3);
+		System.out.println(solution[0]+","+solution[1]);
+	}
+	public static int[] twoSum(int[] nums, int target) {
+	        int[] arr = new int[2];
+			if(nums.length == 0 || nums.length == 1) {
+				//return arr
+				 throw new IllegalArgumentException("Input array too small");
+			} //again, told there is at least one solution which means
+	        //there must be at least 2 numbers in the array
+	        for(int i=0; i<nums.length; i++){
+	        	//.length not .length(); 
+	            for(int y=i+1; y<nums.length; y++){
+	                if(nums[i]+nums[y]==target){
+	                    arr[0]=i;
+	                    arr[1]=y;
+	                    return arr;
+	                }
+	            }
+	        }
+	        //did this because we have to return something, but are promised there exists at least one solution
+	        //return arr;
+	        //would be better to throw an NIAE
+	        throw new IllegalArgumentException("No solution exists");
+	    }
+	}
