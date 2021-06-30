b047ba363b09f81a7ce4a9b734429b2cce2af661
Diana Del Gaudio <ddelgaudio347@gmail.com>
2021-06-30 16:29:13 -0400

    interview question

---
+++ introJava/src/Photos/technicalExercise.java
@@ -0,0 +1,49 @@
+package Photos;
+import java.util.*;
+
+public class technicalExercise {
+	
+	public static void main(String[]args) {
+
+/*
+3 params
+- 3 sides of triangle
+
+
+
+input: 3 sides --> not necessarily int
+output: is valid triangle or not --> boolean Y/N
+error conditions: if any of the sides are not a number --> ex: "hey"
+
+
+isValid(side a, side b, side c)
+if(a b or c is not a number)
+--> return N
+if any of the sides are negative, return N
+else{
+	-- side + side > base
+
+	-- 1 1 2
+	-- 1 1 1.5
+	-- 1 1.5 1
+	-- 1 1 1
+	get the longest side 
+	 	a > b & a > c , then base would be a .... 
+		if(a > b && a > c) then base = a
+		else if (b>a and b > c) then base = c
+		else (base = c)
+
+	if the two smaller sides (not the base) get added together and are > base, return Y
+	else
+	(the two shorter sides added together !> base) --> then return N
+
+	
+}
+
+
+*/
+
+
+	}
+
+}
